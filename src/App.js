3dd1d55f7383af80c3d3566a2d91ca5655fc03a6
tanushvinay <tanush.vinay@gmail.com>
2020-06-24 12:02:21 -0700

    Init Componenets and Api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Chart, Cards, CountryPicker } from './components';
 import './App.css';
+import { fetchdata } from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  async componentDidMount(){
+    const data = await fetchdata();
+
+    console.log(data);
+  }
+  render(){
+    return(
+      <div className="container">
+        <Cards/>
+        <CountryPicker/>
+        <Chart/>
+      </div>
+    )
+  }
 }
+
 
 export default App;
